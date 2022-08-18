b94fc3f6d7f5b605c6b31b9ae9a57c9675de02a3
michel-racine <91474769+michel-racine@users.noreply.github.com>
2022-08-18 13:33:00 -0400

    Create automatic_keyboard_demo.ps1

---
+++ automatic_keyboard_demo.ps1
@@ -0,0 +1,15 @@
+# pops open notepad and says hello
+# for special key formats see: 
+# https://msdn.microsoft.com/en-us/library/system.windows.forms.sendkeys.send(v=vs.110).aspx
+
+add-type -AssemblyName microsoft.VisualBasic;
+add-type -AssemblyName System.Windows.Forms;
+
+notepad; start-sleep 3;
+
+$s = "hello world";
+
+for($i=0;$i -lt $s.length;$i++){
+  [System.Windows.Forms.SendKeys]::SendWait($s[$i]);
+  start-sleep -milliseconds 250;
+}
