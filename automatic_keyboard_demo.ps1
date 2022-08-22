3ec6c6310de548d4503eccd8143ef2bee2b34311
michel-racine <91474769+michel-racine@users.noreply.github.com>
2022-08-21 21:44:06 -0400

    Update automatic_keyboard_demo.ps1

--- automatic_keyboard_demo.ps1
+++ automatic_keyboard_demo.ps1
@@ -2,7 +2,6 @@
 # for special key formats see: 
 # https://msdn.microsoft.com/en-us/library/system.windows.forms.sendkeys.send(v=vs.110).aspx
 
-add-type -AssemblyName microsoft.VisualBasic;
 add-type -AssemblyName System.Windows.Forms;
 
 notepad; start-sleep 3;
